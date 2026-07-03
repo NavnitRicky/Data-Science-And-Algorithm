aa03b9829e33c7272d98b8c56e16bb59fe7a6366
Navnit kumar <2300090359@kluniversity.in>
2026-07-03 16:11:18 +0530

    Time: 0 ms (100%), Space: 45.4 MB (50.67%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -4,21 +4,29 @@ class Solution {
         if (head == null) return null;
 
         ListNode i = head;
-        ListNode j = head.next;
+        // ListNode j = head.next;
 
-        while (j != null) {
+        // while (j != null) {
 
-            if (i.val == j.val) {
-                j = j.next;
-            } else {
-                i.next = j;
-                i = j;
-                j = j.next;
+        //     if (i.val == j.val) {
+        //         j = j.next;
+        //     } else {
+        //         i.next = j;
+        //         i = j;
+        //         j = j.next;
+        //     }
+
+        // }
+
+        // i.next = null;
+        while(i!=null&&i.next!=null){
+            if(i.val==i.next.val){
+                i.next=i.next.next;
             }
-
+            else{
+                i=i.next;
+            }
         }
-
-        i.next = null;
 
         return head;
     }
